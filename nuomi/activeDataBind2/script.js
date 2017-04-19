029eb621e2804c09a17f4586c8ea9eae5290ce8f
guoruihe <guorui.he.seu@gmail.com>
2017-04-19 14:59:33 +0800

    finish task2

--- nuomi/activeDataBind2/script.js
+++ nuomi/activeDataBind2/script.js
@@ -1,133 +1,118 @@
-function Event(element) {
-    this.init(element);
-};
-
-Event.prototype = {
-    constructor: Event,
-    init: function(element) {
-        this.element = (element && element.nodeType == 1) ? 　element : document;
-        return this;
-    },
-
-    on: function(type, callback) {
-        var self = this;
-        if (self.element.addEventListener) { // 标准浏览器下
-            self.element.addEventListener(type, callback, false);
-        } else if (self.element.attachEvent) { // IE
-            if (isNaN(self.element[type])) {
-                self.element[type] = 0;
-            }
-            var fun = function(evt) {
-                evt = evt ? evt : window.event;
-                if (evt.propertyName == type) {
-                    callback.call(self.element);
-                }
-            }
-            self.element.attachEvent('onpropertychange', fun);
-            // 在元素上存储绑定回调，方便移除事件绑定
-            if (!self.element['callback' + callback]) {
-                self.element['callback' + callback] = fun;
-
-            }
-        } else {
-            self.element.attachEvent('on' + type, callback);
-        }
-        return self;
-    },
-
-    removeEvent: function(type, callback) {
-        var self = this;
-        if (self.element.removeEventListener) {
-            self.element.removeEventListener(type, callback, false);
-        } else if (self.element.detachEvent) {
-            // 移除对应的自定义属性监听
-            self.element.detachEvent('onpropertychange', self.element['callback' + callback]);
-            // 删除储存在 DOM 上的自定义事件的回调
-            self.element['callback' + callback] = null;
-
-        } else {
-            self.element.detachEvent('on' + type, callback);
-        }
-        return self;
-    },
-
-    triggerEvent: function(type) {
-        var self = this;
-        if (self.element.dispatchEvent) { // 标准浏览器下
-            // 创建事件
-            var evt = document.createEvent('Event');
-            // 定义事件的类型
-            evt.initEvent(type, true, true);
-            // 触发事件
-            self.element.dispatchEvent(evt);
-        } else if (self.element.fireEvent) { // IE
-            self.element[type]++;
-        }
-        return self;
-    }
-
-}
-
+"use strict";
+// 观察者构造函数
 function Observer(data) {
     this.data = data;
-    this.bindData(data);
+    this.walk(data);
+    this.$p = Array.prototype.slice.call(arguments, 1)[0] || 'data'; // 实现事件冒泡储存父级 名字,传入参数的第二个参数，不存在即为data
+    //console.log(this.$p);
     this.eventsBus = new Event();
-};
+}
 
 let p = Observer.prototype;
 
-p.addEvent = function(onEvent) {};
-
-p.bindData = function(obj) {
+// 此函数用于深层次遍历对象的各个属性
+// 采用的是递归的思路
+// 因为我们要为对象的每一个属性绑定getter和setter
+p.walk = function(obj) {
     let val;
     for (let key in obj) {
+        // 这里为什么要用hasOwnProperty进行过滤呢？
+        // 因为for...in 循环会把对象原型链上的所有可枚举属性都循环出来
+        // 而我们想要的仅仅是这个对象本身拥有的属性，所以要这么做。
         if (obj.hasOwnProperty(key)) {
             val = obj[key];
+            console.log(key);
+            // 这里进行判断，如果还没有遍历到最底层，继续new Observer
             if (typeof val === 'object') {
                 new Observer(val);
-            };
-            this.convert(key, val);
+            }
         }
-    };
+        this.convert(key, val);
+    }
 };
 
-// 设置每一个值的属性
 p.convert = function(key, val) {
+    //Object.defineProperty(obj, prop, descriptor)方法接收三个参数：需要添加或修改属性的对象，属性名称，属性描述options。
+    let _this = this;
     Object.defineProperty(this.data, key, {
         enumerable: true,
         configurable: true,
         get: function() {
             console.log('你访问了' + key);
-            return val;
+            return val
         },
         set: function(newVal) {
+            console.log('你设置了' + key);
+            console.log('新的' + key + ' = ' + newVal)
+                //触发$watch函数
+            _this.eventsBus.emit(key, val, newVal);
+            if (newVal === val) return;
+
+            val = newVal
+
             if (typeof newVal === 'object') {
                 new Observer(newVal);
-            };
-            console.log('你设置了' + key);
-            console.log('新的值为' + key + '=' + newVal);
-            if (newVal === val) return;
-            val = newVal;
+            }
         }
-    });
+    })
 };
-
 p.$watch = function(attr, callback) {
+
     this.eventsBus.on(attr, callback);
-};
+}
 
-let data = {
-    name: "licnjdnvjdn",
-    age: "24",
-    city: "beijing"
-};
+function Event() {
+    this.events = {};
+}
+Event.prototype.on = function(attr, callback) {
 
-let app = new Observer(data);
+    if (this.events[attr]) {
 
-app.$watch('age', function(oldVal, newVal) {
-    console.log('我的年龄变了，原来是: ${oldVal}岁，现在是：$(newVal岁了)');
+        this.events[attr].push(callback);
+
+
+    } else {
+        this.events[attr] = [callback];
+    }
+}
+Event.prototype.off = function(attr) {
+    for (let key in this.events) {
+        if (this.events.hasOwnProperty(key) && key === attr) {
+            delete this.events[key];
+        }
+    }
+}
+Event.prototype.emit = function(attr, ...arg) {
+    //console.log(attr);
+    this.events[attr] && this.events[attr].forEach(function(item) {
+            item(...arg);
+        })
+        //console.log(this,"分割",this.events[attr]);
+}
+let app1 = new Observer({
+    name: {
+        lastName: 'liang',
+        firstName: 'shaofeng'
+    },
+    age: 25
 });
 
-app.$watch('age', function(oldVal, newVal) {
-    console.log('我的年龄真的变了。竟然年轻了${oldVal-newVal}岁');
+app1.$watch('age', function(oldVal, newVal) {
+    console.log(`我的年龄变了，原来是: ${oldVal}岁，现在是：${newVal}岁了`)
+})
+
+app1.$watch('name', function(oldVal, newVal) {
+    console.log(`我的年龄真的变了诶，竟然年轻了${oldVal - newVal}岁`)
+})
+
+let app2 = new Observer({
+    name: {
+        firstName: 'shaofeng',
+        lastName: 'liang'
+    },
+    age: 25
+});
+
+app2.$watch('name', function(newName) {
+    console.log('我的姓名发生了变化，可能是姓氏变了，也可能是名字变了。')
 });
